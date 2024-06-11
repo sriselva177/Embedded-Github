efc1e254648ac59d1f8ad6b7fe687a2ffd3b7c1d
sriselva177 <sriselvaprakash_177@yahoo.com>
2024-06-11 22:37:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Embedded-Github
+ Embedded Basic Sample Codes
---
+++ bitManipulation.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/*
+A scenario where bit 3:3 be checked where it SET or RESET.
+If it is Set, the bit will be reset
+*/
+typedef unsigned char UInt8;        //
+typedef unsigned short int UInt16;
+typedef unsigned int UInt32;
+
+#define BITMASK     0x08
+
+void main()  // no return from function
+{
+UInt8 regVal = 0xDA;
+
+if (BITMASK & regVal)
+{
+    printf("The bit is in SET state\n");
+}
+
+else
+{
+    printf("The bit is in RESET state\n");
+}
+
+regVal &= ~BITMASK;
+printf("The register value after reset bit BITMASK is %.2x\n", regVal);
+
+}
